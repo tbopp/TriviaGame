6bc09e8b8c3d12ba07d368c6af0e7ffc67210980
byt3_h34d <mr.tybopp@gmail.com>
2018-07-06 16:10:32 -0500

    Experimenting with timer creation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 // Setting timer to 60
 
-var counter = 0;
+// var counter = 0;
 
-function setup() {
-    var timer = select('#timer');
-    timer.html(counter);
+// function setup() {
+//     var timer = select('#timer');
+//     timer.html(counter);
 
-    function timeIt() {
-        counter++;
-        timer.html(counter);
-    }
-    setInterval(timeIt, 1000);
+//     function timeIt() {
+//         counter++;
+//         timer.html(counter);
+//     }
+//     setInterval(timeIt, 1000);
 
-};
+// };
 
 
 // var timer = 60;
@@ -24,3 +24,34 @@ function setup() {
 
 // $("#timmer").html('60');
 //  setInterval(timeIt, 1000);
+
+
+var time = 60;
+var running = 0;
+
+function startPause(){
+ if (running == 0){
+    running = 1; 
+    decrement();
+    document.getElementById("start").innerHTML = "Pause";
+
+ }else{
+     running = 0;
+     document.getElementById("start").innerHTML = "Resume";
+ }
+}
+   
+function reset(){
+running = 0;
+time = 60;
+document.getElementById("start").innerHTML = "Start";
+}
+
+function decrement(){
+setTimeour(function(){
+    time--;
+    var secs = Math.floor(time/1000);
+    document.getElementById("timer").innerHTML = secs
+
+})
+}
